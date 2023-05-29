fe5339eaa74eb2dd4cb492937ede530dee075faf
Parvin-Gasimli <parvinzga@code.edu.az>
2023-05-29 18:52:58 +0400

    added

--- test3/test2/src/index.js
+++ test3/test2/src/index.js
@@ -5,7 +5,7 @@ import "./styles.css";
 import "bulma/css/bulma.min.css";
 
 import { Provider } from "react-redux";
-import { store } from "../src/store/index.js";
+import { store } from "./store/index.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
