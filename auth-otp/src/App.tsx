9098aea5005a12295732fea014b7314ebb73297d
Parvin-Gasimli <parvinzga@code.edu.az>
2023-05-29 23:39:25 +0400

    Some CODE add

---
+++ auth-otp/src/App.tsx
@@ -0,0 +1,147 @@
+import {
+    AuthBindings,
+    Authenticated,
+    GitHubBanner,
+    Refine,
+} from "@refinedev/core";
+import {
+    notificationProvider,
+    ThemedLayoutV2,
+    ErrorComponent,
+    RefineThemes,
+} from "@refinedev/antd";
+import dataProvider from "@refinedev/simple-rest";
+import routerProvider, {
+    NavigateToResource,
+    CatchAllNavigate,
+    UnsavedChangesNotifier,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { ConfigProvider } from "antd";
+
+import "@refinedev/antd/dist/reset.css";
+
+import { PostList, PostCreate, PostEdit, PostShow } from "pages/posts";
+import { Login } from "pages/login";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+const App: React.FC = () => {
+    const authProvider: AuthBindings = {
+        login: async ({ gsmNumber, code }) => {
+            if (code === "1234") {
+                localStorage.setItem("gsmNumber", gsmNumber);
+                return {
+                    success: true,
+                    redirectTo: "/",
+                };
+            }
+
+            return {
+                success: false,
+                error: new Error("Login code: 1234"),
+            };
+        },
+        logout: async () => {
+            localStorage.removeItem("gsmNumber");
+            return {
+                success: true,
+                redirectTo: "/login",
+            };
+        },
+        onError: async (error) => {
+            console.error(error);
+            return { error };
+        },
+        check: async () =>
+            localStorage.getItem("gsmNumber")
+                ? {
+                      authenticated: true,
+                  }
+                : {
+                      authenticated: false,
+                      redirectTo: "/login",
+                  },
+        getPermissions: async () => ["admin"],
+    };
+
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <ConfigProvider theme={RefineThemes.Blue}>
+                <Refine
+                    dataProvider={dataProvider(API_URL)}
+                    authProvider={authProvider}
+                    routerProvider={routerProvider}
+                    resources={[
+                        {
+                            name: "posts",
+                            list: "/posts",
+                            show: "/posts/show/:id",
+                            edit: "/posts/edit/:id",
+                            create: "/posts/create",
+                        },
+                    ]}
+                    notificationProvider={notificationProvider}
+                    options={{
+                        syncWithLocation: true,
+                        warnWhenUnsavedChanges: true,
+                    }}
+                >
+                    <Routes>
+                        <Route
+                            element={
+                                <Authenticated
+                                    fallback={<CatchAllNavigate to="/login" />}
+                                >
+                                    <ThemedLayoutV2>
+                                        <Outlet />
+                                    </ThemedLayoutV2>
+                                </Authenticated>
+                            }
+                        >
+                            <Route
+                                index
+                                element={
+                                    <NavigateToResource resource="posts" />
+                                }
+                            />
+
+                            <Route path="/posts">
+                                <Route index element={<PostList />} />
+                                <Route path="create" element={<PostCreate />} />
+                                <Route path="edit/:id" element={<PostEdit />} />
+                                <Route path="show/:id" element={<PostShow />} />
+                            </Route>
+                        </Route>
+
+                        <Route
+                            element={
+                                <Authenticated fallback={<Outlet />}>
+                                    <NavigateToResource resource="posts" />
+                                </Authenticated>
+                            }
+                        >
+                            <Route path="/login" element={<Login />} />
+                        </Route>
+
+                        <Route
+                            element={
+                                <Authenticated>
+                                    <ThemedLayoutV2>
+                                        <Outlet />
+                                    </ThemedLayoutV2>
+                                </Authenticated>
+                            }
+                        >
+                            <Route path="*" element={<ErrorComponent />} />
+                        </Route>
+                    </Routes>
+                    <UnsavedChangesNotifier />
+                </Refine>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+};
+
+export default App;
